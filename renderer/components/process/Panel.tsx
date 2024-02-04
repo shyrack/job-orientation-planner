7a09bca7d72306844a4fddab3b5e6c1c046d9c13
Florian Jahn <CodeBucketCore@gmail.com>
2024-02-04 20:06:21 +0100

    added panel for displaying text | WIP

--- renderer/components/process/Panel.tsx
+++ renderer/components/process/Panel.tsx
@@ -1,11 +1,21 @@
+import { Card, styled } from "@mui/material";
 import React from "react";
 import { ProcessDefinition } from "../../model/process/definition/ProcessDefinition";
-import { Card } from "@mui/material";
+
+const ProcessPanelCard = styled(Card)(({ theme }) => ({
+  display: "flex",
+  height: "150px",
+  width: "150px"
+}));
 
 type PanelProps = {
   processDefinition: ProcessDefinition;
 };
 
 export default function Panel(props: PanelProps) {
-  return <Card></Card>;
+  const { processDefinition } = props;
+
+  const name = React.useMemo(() => processDefinition.getName(), [processDefinition.getName]);
+
+  return <ProcessPanelCard>{name}</ProcessPanelCard>;
 }
