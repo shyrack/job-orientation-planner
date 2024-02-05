00e3a80754744cb2ccc6a157ae5f629f2e088b95
Florian Jahn <CodeBucketCore@gmail.com>
2024-02-05 10:36:57 +0100

    fixed some issue related to any typing

--- renderer/model/process/definition/ProcessDefinition.ts
+++ renderer/model/process/definition/ProcessDefinition.ts
@@ -1,7 +1,7 @@
 export abstract class ProcessDefinition {
   constructor(private name: string) {}
 
-  public abstract execute();
+  public abstract execute(): unknown;
 
   public getName() {
     return this.name;
