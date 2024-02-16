2cf83e0dfd06bbe205acb002ccd31ab7fcd41bd7
Florian Jahn <CodeBucketCore@gmail.com>
2024-02-16 11:46:14 +0100

    added documentation to useAppState React hook

--- renderer/utils/hooks.ts
+++ renderer/utils/hooks.ts
@@ -1,6 +1,20 @@
 import React from "react";
 import { AppState, AppStateContext, AppStateModifier } from "../model/state/AppState";
 
+/**
+ * A React hook for accessing the apps global state from all functional components.
+ *
+ * @author Florian Jahn
+ *
+ * @example // Basic usage of useAppState hook and accessing part of app's global state.
+ * const { accessedSubState: someStuff, modifyAppState } = useAppState((appState) => appState.someStuff);
+ *
+ * @example // Basic modifying of app's global state by using the returned modifyAppState function.
+ * modifyAppState((appState) => { appState.someStuff = "Some new stuff"; });
+ *
+ * @param accessor A function that is given the app's global state and returns part of it.
+ * @returns An object containing the returned sub state of the accessor and a modifying method for safely changing app's global state.
+ */
 export function useAppState<T>(accessor: (appState: AppState) => T) {
   const { setState, state: appState } = React.useContext(AppStateContext);
   const [accessedSubState, setAccessedSubState] = React.useState(accessor(appState));
