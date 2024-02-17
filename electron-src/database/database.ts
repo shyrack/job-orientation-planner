188aa5ca7a2c3bbeed26c6b788a530a55b8a0f56
Florian Jahn <CodeBucketCore@gmail.com>
2024-02-17 14:24:27 +0100

    build the bridge

--- electron-src/database/database.ts
+++ electron-src/database/database.ts
@@ -3,7 +3,7 @@ import { IpcMainEvent, ipcMain } from "electron";
 function onCreateDatabase(event: IpcMainEvent, filepath: string) {
   event.sender; // To cure es-lint shit
   console.log("filepath", filepath);
-  setTimeout(() => ipcMain.emit("database-creation", false), 5000);
+  setTimeout(() => event.sender.send("database-creation", false), 5000);
 }
 
 export function registerEventListeners() {
