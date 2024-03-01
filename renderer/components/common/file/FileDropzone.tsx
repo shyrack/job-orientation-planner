9708b9d67f2faee64127b563927ac8e981a51a2f
Florian Jahn <CodeBucketCore@gmail.com>
2024-03-01 09:30:28 +0100

    added base for FileDropzone component

--- renderer/components/common/file/FileDropzone.tsx
+++ renderer/components/common/file/FileDropzone.tsx
@@ -1,19 +1,39 @@
 import { Paper, PaperProps } from "@mui/material";
-import React from "react";
 import _ from "lodash";
+import React from "react";
 
 type FileDropzoneProps = {
   rootPaperProps: PaperProps;
+  validFileTypes: Array<string>;
 };
 
 export default function FileDropzone(props: FileDropzoneProps) {
-  const { rootPaperProps } = props;
+  const { rootPaperProps, validFileTypes } = props;
+
+  const normalizedValidFileTypes = React.useMemo(
+    () =>
+      _.map(validFileTypes, (validFileType) => {
+        return validFileType.startsWith(".") ? validFileType : "." + validFileType;
+      }),
+    [validFileTypes]
+  );
 
   const onFileDrop = React.useCallback((event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
 
     const droppedItems = event.dataTransfer.items;
-    const droppedItemsCount = _.size(droppedItems);
+    const validFiles = _.filter(droppedItems, (droppedItem) => {
+      if (droppedItem.kind === "file") {
+        const droppedFile = droppedItem.getAsFile();
+        const droppedFileName = droppedFile?.name;
+
+        return _.some(normalizedValidFileTypes, (normalizedValidFileType) =>
+          droppedFileName?.endsWith(normalizedValidFileType)
+        );
+      } else {
+        return false;
+      }
+    });
   }, []);
 
   return <Paper {...rootPaperProps} onDrop={onFileDrop}></Paper>;
