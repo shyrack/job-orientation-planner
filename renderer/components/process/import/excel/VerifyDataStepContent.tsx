67f9f60381670bd0c95390fcd82d4df20981cf36
Florian Jahn <CodeBucketCore@gmail.com>
2024-03-08 23:57:42 +0100

    reworked some stylings and function parameters

--- renderer/components/process/import/excel/VerifyDataStepContent.tsx
+++ renderer/components/process/import/excel/VerifyDataStepContent.tsx
@@ -5,13 +5,6 @@ import { useAppState } from "../../../../utils/hooks";
 import FlexContainer from "../../../common/flex/FlexContainer";
 import FlexSpacer from "../../../common/flex/FlexSpacer";
 
-const WorksheetStepper = styled(Stepper)({
-  display: "flex",
-  flexGrow: 0,
-  flexShrink: 1,
-  flexWrap: "wrap"
-});
-
 const VerifyDataStepWrapper = styled(FlexContainer)({
   flexDirection: "column"
 });
@@ -21,24 +14,22 @@ const StepperButtonWrapper = styled(FlexContainer)({
   flexShrink: 1
 });
 
+const ButtonStepperSpacer = styled(FlexSpacer)({
+  flexGrow: 1
+});
+
 export default function VerifyDataStepContent(props: StepContentProps) {
   const { accessedSubState: worksheets } = useAppState((appState) => appState.excelFileImportProcessState.worksheets);
   const worksheetCount = React.useMemo(() => _.size(worksheets), [worksheets]);
   const [activeStep, setActiveStep] = React.useState(0);
 
-  const onNextButtonClick = React.useCallback(
-    (event: React.MouseEvent<HTMLButtonElement>) => {
-      setActiveStep(activeStep + 1);
-    },
-    [activeStep, setActiveStep]
-  );
+  const onNextButtonClick = React.useCallback(() => {
+    setActiveStep(activeStep + 1);
+  }, [activeStep, setActiveStep]);
 
-  const onBackButtonClick = React.useCallback(
-    (event: React.MouseEvent<HTMLButtonElement>) => {
-      setActiveStep(activeStep - 1);
-    },
-    [activeStep, setActiveStep]
-  );
+  const onBackButtonClick = React.useCallback(() => {
+    setActiveStep(activeStep - 1);
+  }, [activeStep, setActiveStep]);
 
   return (
     <VerifyDataStepWrapper>
@@ -48,13 +39,14 @@ export default function VerifyDataStepContent(props: StepContentProps) {
             key={`verify-data-step-${worksheet.getFilename()}-${worksheet.getName()}-${index}`}
             completed={index < activeStep}
           >
-            <StepLabel>{`${worksheet.getFilename()} ${worksheet.getName()}`}</StepLabel>
+            <StepLabel>{`${worksheet.getFilename()}: ${worksheet.getName()}`}</StepLabel>
             <StepContent>
               <div>Some very important step content stuff</div>
             </StepContent>
           </Step>
         ))}
       </Stepper>
+      <ButtonStepperSpacer />
       <StepperButtonWrapper>
         <Button disabled={activeStep === 0} onClick={onBackButtonClick}>
           Zurück
