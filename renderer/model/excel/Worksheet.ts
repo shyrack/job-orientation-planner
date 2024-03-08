278aa27239de0fb2fa612352ebe7e96444623e52
Florian Jahn <CodeBucketCore@gmail.com>
2024-03-09 00:32:02 +0100

    added methods for data grid compatibility to work sheet

--- renderer/model/excel/Worksheet.ts
+++ renderer/model/excel/Worksheet.ts
@@ -1,5 +1,10 @@
+import { DataGridProps } from "@mui/x-data-grid";
+import _ from "lodash";
+
 export class Worksheet {
-  constructor(private filename: string, private name: string, private data: Array<string>) {}
+  constructor(private filename: string, private name: string, private data: Array<string>) {
+    console.log("data", data);
+  }
 
   getFilename() {
     return this.filename;
@@ -11,5 +16,34 @@ export class Worksheet {
 
   getData() {
     return this.data;
+  }
+
+  getColumns(): DataGridProps["columns"] {
+    const columns: Array<string> = [];
+
+    _.forEach(this.data, (row) => {
+      const parsedRow = JSON.parse(row);
+      const rowKeys = _.keys(parsedRow);
+
+      _.forEach(rowKeys, (rowKey) => {
+        if (!_.startsWith(rowKey, "_") && !_.includes(columns, rowKey)) {
+          columns.push(rowKey);
+        }
+      });
+    });
+
+    return _.map(columns, (column) => ({ field: column }));
+  }
+
+  getRows() {
+    return _.map(this.data, (row, index) => {
+      const parsedRow = JSON.parse(row);
+
+      if (!parsedRow["id"]) {
+        return { ...parsedRow, id: `parsed-excel-row-${index}` };
+      } else {
+        return parsedRow;
+      }
+    });
   }
 }
