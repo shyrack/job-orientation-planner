b545eb0b3139d0d08c748eceef8b1bb2f72de620
Florian Jahn <CodeBucketCore@gmail.com>
2024-03-08 19:44:57 +0100

    fixed problems with usage of xlsx on linux platforms

--- renderer/components/process/import/excel/ChooseWorksheetStepContent.tsx
+++ renderer/components/process/import/excel/ChooseWorksheetStepContent.tsx
@@ -1,27 +1,31 @@
 import { StepContentProps } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import _ from "lodash";
 import React from "react";
-import xlsx from "xlsx";
+import * as xlsx from "xlsx";
 import { Worksheet } from "../../../../model/excel/Worksheet";
 import { useAppState } from "../../../../utils/hooks";
 
 export default function ChooseWorksheetStepContent(props: StepContentProps) {
   const { accessedSubState: excelFiles } = useAppState((appState) => appState.excelFileImportProcessState.excelFiles);
 
-  const buildWorksheetList = React.useCallback(() => {
-    return _.union(
-      ..._.map(excelFiles, (excelFile) => {
-        const readExcelWorkbook = xlsx.read(excelFile.text);
+  const buildWorksheetList = React.useCallback(async () => {
+    const worksheetMap = await Promise.all(
+      _.map(excelFiles, async (excelFile) => {
+        const excelFileText = await excelFile.text();
+        const readExcelWorkbook = xlsx.read(excelFileText);
+
         return _.map(
           readExcelWorkbook.Sheets,
           (worksheet, worksheetName) => new Worksheet(worksheetName, xlsx.utils.sheet_to_json(worksheet))
         );
       })
     );
+
+    return _.flatten(worksheetMap);
   }, [excelFiles]);
 
   const worksheets = React.useMemo(() => buildWorksheetList(), [buildWorksheetList]);
 
-  // return <DataGrid columns={} />;
-  return null;
+  return <DataGrid columns={[]} rows={[]} />;
 }
