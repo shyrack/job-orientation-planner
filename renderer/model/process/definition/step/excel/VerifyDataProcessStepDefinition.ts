69f3dd6c720704fcd5dc41bbf16512e22798240e
Florian Jahn <CodeBucketCore@gmail.com>
2024-03-15 13:01:52 +0100

    added completion for process steps

--- renderer/model/process/definition/step/excel/VerifyDataProcessStepDefinition.ts
+++ renderer/model/process/definition/step/excel/VerifyDataProcessStepDefinition.ts
@@ -1,13 +1,29 @@
 import VerifyDataStepContent from "../../../../../components/process/import/excel/VerifyDataStepContent";
+import { AppState } from "../../../../state/AppState";
 import { ProcessStepDefinition } from "../ProcessStepDefinition";
 
 export class VerifyDataProcessStepDefinition extends ProcessStepDefinition {
+  private callback?: (valid: boolean, id?: string) => void;
+  private valid?: boolean;
+
   constructor() {
     super("Daten verifizieren");
   }
 
-  public validate() {
-    return false;
+  private onAppStateUpdate(appState: AppState) {
+    const updatedValidationResult = appState.excelFileImportProcessState.reviewedWorksheets ?? false;
+
+    if (this.callback !== undefined && updatedValidationResult !== this.valid) {
+      this.callback(updatedValidationResult, this.id);
+      this.valid = updatedValidationResult;
+    }
+  }
+
+  public validate(callback?: (valid: boolean, id?: string) => void) {
+    this.callback = callback;
+    AppState.instance?.subscribe(this.onAppStateUpdate.bind(this), true);
+
+    return AppState.instance?.excelFileImportProcessState.reviewedWorksheets ?? false;
   }
 
   public execute() {}
